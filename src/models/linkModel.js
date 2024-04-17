587a215b457097aafa42ee09e3fbbbb0f8bb0f99
Rishit-Thaper <rishitthaper5329@gmail.com>
2024-04-18 00:13:31 +0530

    Add linkModel with required fields and timestamps

--- src/models/linkModel.js
+++ src/models/linkModel.js
@@ -1,18 +1,23 @@
 import mongoose, { Schema } from "mongoose";
-const linkSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const linkSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  url: {
-    type: String,
-    required: true,
-  },
-  user_id: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const Link = mongoose.model("Link", linkSchema);
